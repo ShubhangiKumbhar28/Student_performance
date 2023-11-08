e2eef05955f628a0e1b2b30dee7728e48fd824b1
Shubhangi Kumbhar <Shubhangi2728@gmail.com>
2023-11-08 21:34:46 +0530

    Trained Model

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformantion, DataTransformantionConfig
-
+from src.components.model_train import ModelTrainer,ModelTrainerConfig
 # Initialize Data Ingestion Configuration
 @dataclass
 class DataIngestionConfig:
@@ -55,4 +55,10 @@ if __name__ =="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation = DataTransformantion()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr,_ =data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+
+
